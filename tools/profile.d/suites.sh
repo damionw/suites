ae896279f6dad2b24e929a70add6f36fffcb975f
dkw <dkw@renre.com>
2017-06-26 17:34:00 -0300

    Add prototype launcher for profile scripts

---
+++ tools/profile.d/suites.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+folders="$(
+    find "${HOME}/.suites/" -maxdepth 1 -mindepth 1 -type d ! -name '.*' -print | while read folder
+    do
+        find "${folder}" -maxdepth 1 -mindepth 1 -type f -exec grep -q -E '^[[:alnum:]]+::run[\(][\)]' {} \; -exec echo ${folder} \;
+    done | sort -u
+)"
+
+while read folder
+do
+    if [ -d "${folder}" ]
+    then
+        suites --suite="${folder}" --start
+    fi
+done <<< "${folders}"
